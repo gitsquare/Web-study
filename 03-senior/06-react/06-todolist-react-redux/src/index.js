778a329afac4be49b9ea0319048717d1d4fdda86
jack <2489770898@qq.com>
2019-04-22 13:49:12 +0800

    09-router

--- 03-senior/06-react/06-todolist-react-redux/src/index.js
+++ 03-senior/06-react/06-todolist-react-redux/src/index.js
@@ -8,4 +8,5 @@ import App from './App.js'
 
 //App是整个应用的顶层组件
 //用Provider包裹，设置一个store属性，值是从store目录下index.js文件中导出来的
+//用Provider包裹以后，所有的子组件都可以拿到传进的这个store
 ReactDOM.render(<Provider store={store}><App /></Provider>,document.getElementById('root'))
